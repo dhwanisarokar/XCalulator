77f9b4943c285923828d670cc3220ada7278e620
dhwanisarokar <dhwanisarokar@gmail.com>
2024-03-28 23:16:03 +0530

    impliment calulator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,80 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [displayValue, setDisplayValue] = useState("");
+  const [expression, setExpression] = useState("0");
+
+  const handleBtnClick = (value) => {
+    setDisplayValue("");
+    if (value === "=") {
+      if (value === "") setDisplayValue("Error");
+      else evaluateExpression();
+    } else if (value === "C") {
+      setDisplayValue("");
+      setExpression("0");
+    } else {
+      updateValue(value);
+    }
+  };
+
+  const updateValue = (value) => {
+    if (value === "0" && displayValue === "0") return;
+
+    if (expression === "0" && ["+", "-", "*", "/"].includes(value)) {
+      setExpression(`0${value}`);
+    } else {
+      setExpression((prevState) =>
+        prevState === "0" ? value : prevState + value
+      );
+    }
+  };
+
+  const evaluateExpression = () => {
+    try {
+      const result = evaluteBODMAS(expression);
+      setDisplayValue(result.toString());
+    } catch (error) {
+      console.log("Error while calulation", error.message);
+      setDisplayValue("Error");
+    }
+  };
+
+  const evaluteBODMAS = (exp) => {
+    if (exp === "0/0") return "NaN";
+    if (exp.includes("/0")) return "Infinity";
+
+    return eval(exp);
+  };
+
+  return (
+    <>
+      <div className="calulator">
+        <h1>React Calulator</h1>
+
+        <input type="text" value={expression} readOnly />
+        <div className="answser">{displayValue}</div>
+        <div className="buttons">
+          <button onClick={() => handleBtnClick("7")}>7</button>
+          <button onClick={() => handleBtnClick("8")}>8</button>
+          <button onClick={() => handleBtnClick("9")}>9</button>
+          <button onClick={() => handleBtnClick("+")}>+</button>
+          <button onClick={() => handleBtnClick("4")}>4</button>
+          <button onClick={() => handleBtnClick("5")}>5</button>
+          <button onClick={() => handleBtnClick("6")}>6</button>
+          <button onClick={() => handleBtnClick("-")}>-</button>
+          <button onClick={() => handleBtnClick("1")}>1</button>
+          <button onClick={() => handleBtnClick("2")}>2</button>
+          <button onClick={() => handleBtnClick("3")}>3</button>
+          <button onClick={() => handleBtnClick("*")}>*</button>
+          <button onClick={() => handleBtnClick("C")}>C</button>
+          <button onClick={() => handleBtnClick("0")}>0</button>
+          <button onClick={() => handleBtnClick("=")}>=</button>
+          <button onClick={() => handleBtnClick("/")}>/</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
